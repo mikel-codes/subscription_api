d155ac1b8ade75ebaa05e3b9f32e9d04a4ed8b62
mikel-codes <mikezone_0906@yahoo.com>
2025-08-05 17:26:20 +0100

    Integerate routes to app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from db import db, init_db
-
+from routes.auth import auth_bp
+from routes.subscription import sub_bp
+from routes.plans import plans_bp
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
@@ -10,7 +12,9 @@ with app.app_context():
     init_db()
 
 
-
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(plans_bp, url_prefix="/plans")
+app.register_blueprint(sub_bp, url_prefix="/subscriptions")
 
 if __name__ == "__main__":
     app.run(debug=True)
