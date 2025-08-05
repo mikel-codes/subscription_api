0c3b252a155ff2669e93c0edc9d7ca0978a9c227
mikel-codes <mikezone_0906@yahoo.com>
2025-08-05 17:24:15 +0100

    Add comments for details

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
+#Explicity define fields here to simplify model definition
 Model = db.Model
 Column = db.Column
 Integer = db.Integer
@@ -13,8 +13,10 @@ Numeric = db.Numeric
 ForeignKey = db.ForeignKey
 Enum = db.Enum
 relationship = db.relationship
+JSON = db.JSON
 Index = db.Index
 
+
 def init_db():
-    import models  
+    from models import User, Plan, Subscription  # to avoid circular imports
     db.create_all()
