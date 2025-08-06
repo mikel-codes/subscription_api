c9613c161176429da3daa1bd60f424103dc94b17
mikel-codes <mikezone_0906@yahoo.com>
2025-08-06 02:23:59 +0100

    Update app.py app_context  and imports

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_migrate import Migrate
 from db import db, init_db
-from routes.auth import auth_bp
-from routes.subscription import sub_bp
-from routes.plans import plans_bp
+from routes import auth_bp, sub_bp, plans_bp
+
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
@@ -11,13 +10,13 @@ app.config.from_object("config.Config")
 db.init_app(app) 
 migrate = Migrate(app, db)
 
-with app.app_context():
-    init_db()
-
-
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(plans_bp, url_prefix="/plans")
 app.register_blueprint(sub_bp, url_prefix="/subscriptions")
+
+if app.config["ENVIRONMENT"] == "development":
+    with app.app_context():
+        init_db()
 
 if __name__ == "__main__":
     app.run(debug=True)
